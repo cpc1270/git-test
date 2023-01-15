3ca6d9e3cdb97338c374c9549ec44120504eb431
cpc1270 <3370562476@qq.com>
2023-01-15 13:03:15 +0800

    second commit

--- src/main/java/indi/cpc1270/git/GitTest.java
+++ src/main/java/indi/cpc1270/git/GitTest.java
@@ -8,5 +8,6 @@ package indi.cpc1270.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
