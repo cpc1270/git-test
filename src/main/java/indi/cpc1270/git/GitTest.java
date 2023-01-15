ea13b7b52697d1f0da1b1ffdfada53c3278ef8c9
cpc1270 <3370562476@qq.com>
2023-01-15 13:01:08 +0800

    first commit

---
+++ src/main/java/indi/cpc1270/git/GitTest.java
@@ -0,0 +1,12 @@
+package indi.cpc1270.git;
+
+/**
+ * @author cpc1270
+ * @create 2023-01-15-12:56
+ * description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
